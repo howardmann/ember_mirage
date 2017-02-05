d472632f8dee898499a4b3e77a55578a315615a6
howardmann <howardmann27@gmail.com>
2017-02-05 15:21:53 +1100

    Add computed itemCount and checkCount to todo-list

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  itemCount: Ember.computed('model.length',function(){
+    return this.get('model.length');
+  }),
+  checkCount: Ember.computed('model.@each.isComplete', function(){
+    return this.get('model').filterBy('isComplete', false).length;
+  })
 });
